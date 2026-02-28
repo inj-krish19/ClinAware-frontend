e9e40968eea740c1e79d9dad5671f57c1be32c52
inj-krish19 <kglivee19@gmail.com>
2026-02-28 21:54:05 +0530

    Pushing folder structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p className='text-sky-400 font-bold text-2xl'>Everything gonna be alright</p>
+      </div>
+    </>
+  )
+}
+
+export default App
